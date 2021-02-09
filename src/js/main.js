659f388a7bb51cb073271f8edddaa3aed889e5a0
veronica <veronicaweiss77@gmail.com>
2021-02-09 16:57:44 +0100

    gallery

--- src/js/main.js
+++ src/js/main.js
@@ -73,4 +73,57 @@ const TxtRotate = function(el, toRotate, period) {
 
   //планый переход по локальным ссылкам
 
-  
+  //slider
+
+let offset = 0;
+let slideIndex = 1;
+
+const   slides = document.querySelectorAll('.slide'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        wrapper = document.querySelector('.gallery'),
+        width = window.getComputedStyle(wrapper).width,
+        slidesField = document.querySelector('.gallery__photos');
+
+            
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    wrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener('click', () => {
+        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2); 
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+    });
